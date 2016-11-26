19afefd4d8d216845b7f69718988cc4f9d319977
Rubén Ibáñez Carmona <ruben.ibanez@tecnotramit.com>
2016-11-26 16:14:07 +0100

    more on the helpers

--- app/helpers/webpack_helper.rb
+++ app/helpers/webpack_helper.rb
@@ -1,15 +1,40 @@
 module WebpackHelper
-  def webpack_asset_url(asset)
-    if Rails.env.development?
-      "http://localhost:3500#{manifest.fetch(asset)['js']}"
-    else
-      "/assets/#{manifest.fetch(asset)}"
-    end
-  end
-
   def manifest
     @manifest ||= JSON.parse(File.read('webpack-assets.json'))
   rescue
-    fail 'Please run webpack'
+    raise 'Please run webpack'
+  end
+
+  def webpack_js_tag(asset)
+    if Rails.env.development?
+      javascript_include_tag "http://localhost:3500#{manifest.fetch(asset)['js']}"
+    else
+      javascript_include_tag "/assets/#{manifest.fetch(asset)}"
+    end
+  end
+
+  def webpack_css_tag(asset)
+    if Rails.env.development?
+      stylesheet_link_tag "http://localhost:3500#{manifest.fetch(asset)['css']}", media: 'all'
+    else
+      stylesheet_link_tag "/assets/#{manifest.fetch(asset)}", media: 'all'
+    end
+  end
+
+  def all_assets
+    assets = ''
+    manifest.each do |name, _value|
+      manifest[name].each do |type, path|
+        assets << case type
+                  when 'js'
+                    javascript_include_tag path
+                  when 'css'
+                    stylesheet_link_tag path, media: 'all'
+                  else
+                    raise %(#{type} still not implemented)
+                  end
+      end
+    end
+    assets
   end
 end
