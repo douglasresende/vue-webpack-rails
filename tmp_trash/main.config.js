52f5c2e02acc0d23d538e54858714ea19b3575d7
Rubén Ibáñez Carmona <ruben.ibanez@tecnotramit.com>
2016-11-29 09:04:52 +0100

    remove trash

--- tmp_trash/main.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-
-const env = process.env.NODE_ENV || 'development'
-
-module.exports = merge(
-  require('./base.config.js'),
-  require(`./${env}.config.js`)
-)
